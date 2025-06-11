cccfba36616b69332b332e5f665f0273ea5c7d51
AnsirH <ansir0211@gmail.com>
2025-06-11 17:27:56 +0900

    add: sqlite

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+import sqlite3
 
 # 객체 생성
 app = Flask(__name__)
+app.secret_key = 'secret key'
+
+def save_messages(name, email, message):
+    conn = sqlite3.connect("contact.db")
+    c = conn.cursor()
+    c.execute("""
+              insert into messages(name, email, message)
+              values(?, ?, ?)
+              """, (name, email, message))
+    conn.commit()
+    conn.close()    
 
 @app.route("/")
 def index():
@@ -14,6 +26,27 @@ def about():
 @app.route("/projects")
 def projects():
     return render_template("projects.html")
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
+
+        save_messages(name, email, message)
+
+        print(f"제출된 데이터 이름: {name}")
+        print(f"제출된 데이터 email: {email}")
+        print(f"제출된 데이터 메세지: {message}")
+
+        # redirect 시에만 success 표시
+        session["success"] = True
+        return redirect(url_for("contact")) # GET 방식으로 
+    
+    # GET 요청 시 success 표시 후 초기화
+    success = session.pop("success", False)
+    return render_template("contact.html", success=success)
 
 if __name__ == "__main__":
     app.run(debug=True)
